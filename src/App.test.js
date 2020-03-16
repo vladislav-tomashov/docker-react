8e750aefbb538c92666d15989c5a689f04770a0a
Vlad Tomashov <vladislav.tomashov@gmail.com>
2020-03-16 09:13:34 +0200

    adds nginx

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Bye there123!", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Bye there123!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Bye there123! again", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Bye there123!/i);
   expect(linkElement).toBeInTheDocument();
 });
